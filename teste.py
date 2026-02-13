b6152ecc73f2022c8733dba0b2e230d86426727f
laerte <laerte@laerte.com>
2026-02-13 11:05:38 -0300

    teste

---
+++ teste.py
@@ -0,0 +1,28 @@
+print("hello world")
+print(1+1)
+ano=1
+nome="laerte"
+c=1.1
+lista = [a,b,c]
+lista[2]
+tupla=(a,b,c)
+conta_bancarido_cliente
+dicionario = {
+    1:a,
+    10:b,
+    100:c
+}
+dicionario[100]
+meses_ano = {
+    1:"Janeiro",
+    2:"Fevereiro",
+    3:"Marco",
+}
+meses_ano[1]
+
+
+def calcular_soma(a,b):
+    resultado = a + b
+    return resultado
+
+calcular_soma(2, 2)
